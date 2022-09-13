e66aceef873a1b9d0b482e20b49dcc67abe9ea13
Legente <williamvernillo@gmail.com>
2022-09-13 13:34:35 +0200

    Main

--- src/exam/Main.java
+++ src/exam/Main.java
@@ -2,10 +2,10 @@ package exam;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        SuperClass a = new SuperClass();
-        SuperClass b = new SubClass();
 
-        a.print();
-        b.print();
+    }
+
+    public void print(){
+        System.out.println("ciao");
     }
 }
