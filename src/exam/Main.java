dc3bf968d25335e7f9c7654eb8345fda36dd3741
Legente <williamvernillo@gmail.com>
2022-09-13 14:12:13 +0200

    Try sonar

--- src/exam/Main.java
+++ src/exam/Main.java
@@ -4,10 +4,23 @@ package exam;
 
 public class Main {
     public static void main(String[] args) {
-        print();
+        int a = 100;
+        int b = 500;
+        print(a, b);
     }
 
-    public static void print(){
-        Log.info("ciao");
+    public static void print(int a, int b){
+        Log.info("Sommo i due numeri");
+        int c = a + b;
+        Log.info("Il risultato è " + c);
+
+        Log.info("Eseguo la sottrazione dei numeri ");
+        int d = a - b;
+        if (d<0){
+            Log.info("La somma è negativa " + d);
+        } else {
+            Log.info("La somma è positiva " + d);
+        }
+
     }
 }
