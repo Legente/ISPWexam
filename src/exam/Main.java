061205b8a8117111a222730d2f66b7c76bab5fb5
Legente <williamvernillo@gmail.com>
2022-09-13 13:43:42 +0200

    Try sonar

--- src/exam/Main.java
+++ src/exam/Main.java
@@ -1,7 +1,5 @@
 package exam;
 
-import java.util.logging.Logger;
-
 //sonar-scanner.bat -D"sonar.organization=legente" -D"sonar.projectKey=Legente_ISPWexam" -D"sonar.sources=." -D"sonar.host.url=https://sonarcloud.io"
 
 public class Main {
